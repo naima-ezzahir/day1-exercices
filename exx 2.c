aa2182b3a4f6da4cc048eb3e917f6e282ffe53b3
naima <ezzahirnaima575@gmail.com>
2024-09-24 17:04:04 +0200

    exercice_1

---
+++ exx 2.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+float k ,C;
+
+printf("entrer la temperatur en Celsius\n");
+scanf("%f",&C);
+k = C + 273.1;
+    printf("la temperatur en Kelvin:%f\n",k);
+    return 0;
+}
