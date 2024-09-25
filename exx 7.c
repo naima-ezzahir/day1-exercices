5df8bc20da73412f59dae0e5a9e61078649b1d4c
naima-ezzahir <ezzahirnaima575@gmail.com>
2024-09-25 09:48:13 +0200

    Update exx 7.c

---
+++ exx 7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int a ,b ,c;
+ float m;
+printf("entrer le nbr a ");
+scanf("%d",&a);
+printf("entrer le nbr b ");
+scanf("%d",&b);
+printf("entrer le nbr c ");
+scanf("%d",&c);
+m =(a+b+c)/3;
+printf("la moyenne:%f ",m);
+
+    return 0;
+}
